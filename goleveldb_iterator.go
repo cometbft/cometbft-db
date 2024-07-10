25a8f7b121e36ba8da3400333a6a4df40272a56e
Alessandro <alessandro@informal.systems>
2024-07-10 18:12:55 +0200

    changes in goLevelDBIterator Key() impl: - returns the key itself, not a copy. - updated docs to reflect the change

--- goleveldb_iterator.go
+++ goleveldb_iterator.go
@@ -93,11 +93,11 @@ func (itr *goLevelDBIterator) Valid() bool {
 }
 
 // Key implements Iterator.
+// The caller should not modify the contents of the returned slice.
+// To work with the slice, make a copy and modify the copy instead.
 func (itr *goLevelDBIterator) Key() []byte {
-	// Key returns a copy of the current key.
-	// See https://github.com/syndtr/goleveldb/blob/52c212e6c196a1404ea59592d3f1c227c9f034b2/leveldb/iterator/iter.go#L88
 	itr.assertIsValid()
-	return cp(itr.source.Key())
+	return itr.source.Key()
 }
 
 // Value implements Iterator.
