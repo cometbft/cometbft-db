6ff41ace4075233d70559cf8e6f72345bb8f3300
Anton Kaliaev <anton.kalyaev@gmail.com>
2024-09-20 09:38:53 +0400

    update *DBBackend descriptions

--- db.go
+++ db.go
@@ -11,11 +11,13 @@ type BackendType string
 const (
 	// GoLevelDBBackend represents goleveldb (github.com/syndtr/goleveldb - most
 	// popular implementation)
+	//   - UNMAINTANED
 	//   - pure go
 	//   - stable
-	//   - unmaintaned
+	//   - use goleveldb build tag (go build -tags goleveldb)
 	GoLevelDBBackend BackendType = "goleveldb"
 	// CLevelDBBackend represents cleveldb (uses levigo wrapper)
+	//   - DEPRECATED
 	//   - fast
 	//   - requires gcc
 	//   - use cleveldb build tag (go build -tags cleveldb)
@@ -25,22 +27,20 @@ const (
 	MemDBBackend BackendType = "memdb"
 	// BoltDBBackend represents bolt (uses etcd's fork of bolt -
 	// github.com/etcd-io/bbolt)
-	//   - EXPERIMENTAL
-	//   - may be faster is some use-cases (random reads - indexer)
+	//   - DEPRECATED
+	//   - pure go
 	//   - use boltdb build tag (go build -tags boltdb)
 	BoltDBBackend BackendType = "boltdb"
 	// RocksDBBackend represents rocksdb (uses github.com/tecbot/gorocksdb)
-	//   - EXPERIMENTAL
 	//   - requires gcc
 	//   - use rocksdb build tag (go build -tags rocksdb)
 	RocksDBBackend BackendType = "rocksdb"
 	// BadgerDBBackend represents badger (uses github.com/dgraph-io/badger)
-	//   - EXPERIMENTAL
+	//   - pure go
 	//   - use badgerdb build tag (go build -tags badgerdb)
 	BadgerDBBackend BackendType = "badgerdb"
 	// PebbleDBDBBackend represents pebble (uses github.com/cockroachdb/pebble)
-	//   - EXPERIMENTAL
-	//   - use pebbledb build tag (go build -tags pebbledb)
+	//   - pure go
 	PebbleDBBackend BackendType = "pebbledb"
 )
 
